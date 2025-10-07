7d3aa383c47b721707ae4a4b7e1491cb4b8bfa7d
Chad Spratt <internet2.0@gmail.com>
2025-10-07 14:53:41 -0500

    cancel buildings that are in queues other than "started"

--- bottato/build_order.py
+++ bottato/build_order.py
@@ -467,7 +467,7 @@ class BuildOrder(TimerMixin):
     def cancel_damaged_structure(self, unit: Unit, total_damage_amount: float):
         if unit.health_percentage > 0.05:
             return
-        for idx, build_step in enumerate(self.started):
+        for idx, build_step in enumerate(self.started + self.priority_queue + self.static_queue + self.build_queue):
             if build_step.unit_being_built is not None and build_step.unit_being_built is not True and build_step.unit_being_built.tag == unit.tag:
                 build_step.cancel_construction()
                 break
