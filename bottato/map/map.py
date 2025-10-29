192d205531723a550d0185c77dbab3d54a8eb781
Chad Spratt <internet2.0@gmail.com>
2025-10-29 09:05:39 -0500

    add missing import

--- bottato/map/map.py
+++ bottato/map/map.py
@@ -9,7 +9,9 @@ from sc2.units import Units
 from sc2.unit import Unit
 from sc2.bot_ai import BotAI
 # from sc2.pixel_map import PixelMap
-from sc2.position import Point2, Point3
+from sc2.position import Point2
+from sc2.ids.unit_typeid import UnitTypeId
+
 
 from ..mixins import TimerMixin, GeometryMixin
 from .zone import Path, Zone
