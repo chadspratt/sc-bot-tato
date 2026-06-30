5fa533bc3278de1a4f72db92e250046cb5da8e77
Chad Spratt <internet2.0@gmail.com>
2026-06-30 15:15:26 -0500

    fix scv retreat from threats by raising attack_health threshold to 0.9

--- bottato/micro/scv_micro.py
+++ bottato/micro/scv_micro.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from bottato.mixins import GeometryMixin
 from bottato.micro.base_unit_micro import BaseUnitMicro
+from bottato.mixins import GeometryMixin
 
 
 class SCVMicro(BaseUnitMicro, GeometryMixin):
-    attack_health: float = 0.6
+    attack_health: float = 0.9
